6356ff9d93a8cc19b92e141af00fb39c3555386f
muhammadhussain29 <hussainmughal29@gmail.com>
2025-12-06 14:50:04 +0500

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-5xl h-screen w-screen flex justify-center items-center'>AskBox</div>
+  )
+}
+
+export default App
