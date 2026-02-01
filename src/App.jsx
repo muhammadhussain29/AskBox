57917813957837c2c8e3f2ca7046a1984e915cef
muhammadhussain29 <hussainmughal29@gmail.com>
2026-02-01 14:16:40 +0500

    initial commit

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const App = () => {
-  return (
-    <div className='text-5xl h-screen w-screen flex justify-center items-center'>AskBox</div>
-  )
-}
-
-export default App
